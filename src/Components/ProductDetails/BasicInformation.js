1d108c1f515921685f6cc4388ff78cee4cd3a1df
Sofia Duran <durannsofia@gmail.com>
2020-10-28 12:12:03 +0100

    added placeholder data to everypage

---
+++ src/Components/ProductDetails/BasicInformation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Productdetails.css'
+
+export default function BasicInformation() {
+  return (
+    <div className="main-product-info">
+      <div className="image-section">
+        <img className="product-images"></img>
+      </div>
+      <div className="main-data">
+        <h2>Haribo Saft Goldbears</h2>
+        <h3>Mondelez-international</h3>
+      </div>
+    </div>
+  )
+}
