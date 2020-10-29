6a6727ddf9d5816a9c80c572b010ac22f523710e
Sofia Duran <durannsofia@gmail.com>
2020-10-29 07:21:37 +0100

    changed all icons to components

--- src/Components/ProductDetails/Contains.js
+++ src/Components/ProductDetails/Contains.js
@@ -1,20 +1,14 @@
 import React from 'react';
-
-import eggs from '../../icons/contains-green/eggs-green.svg';
-import fats from '../../icons/contains-green/fats-green.svg';
-import fish from '../../icons/contains-green/fish-green.svg';
-import gluten from '../../icons/contains-green/gluten-green.svg';
-import lactose from '../../icons/contains-green/lactose-green.svg';
-import nuts from '../../icons/contains-green/nuts-green.svg';
-import peanuts from '../../icons/contains-green/peanuts-green.svg';
-import sugar from '../../icons/contains-green/sugar-green.svg';
-import vegan from '../../icons/contains-green/vegan-green.svg';
-import { EggIcon, FishIcon, FatsIcon, GlutenIcon, LactoseIcon } from '../../shared/icons';
-
-
-
-
-
+import { 
+  EggIcon,
+  FishIcon, 
+  FatsIcon,
+  GlutenIcon,
+  LactoseIcon,
+  NutsIcon,
+  PeanutsIcon,
+  SugarIcon,
+  VeganIcon } from '../../shared/icons';
 
 export default function Contains() {
   return (
@@ -24,11 +18,11 @@ export default function Contains() {
       <FishIcon/>
       <GlutenIcon/>
       <LactoseIcon/>
-      
-      <img src={nuts}/>
-      <img src={peanuts}/>
-      <img src={sugar}/>
-      <img src={vegan}/>
+      <NutsIcon/>
+      <PeanutsIcon/>
+      <SugarIcon/>
+      <VeganIcon/>
+
     </div>
   )
 }
--- src/shared/icons.js
+++ src/shared/icons.js
@@ -3,3 +3,19 @@ export { ReactComponent as FatsIcon} from '../icons/contains-green/fats-green.sv
 export { ReactComponent as FishIcon} from '../icons/contains-green/fish-green.svg';
 export { ReactComponent as GlutenIcon} from '../icons/contains-green/gluten-green.svg';
 export { ReactComponent as LactoseIcon} from '../icons/contains-green/lactose-green.svg';
+export { ReactComponent as NutsIcon} from '../icons/contains-green/nuts-green.svg';
+export { ReactComponent as PeanutsIcon} from '../icons/contains-green/peanuts-green.svg';
+export { ReactComponent as SugarIcon} from '../icons/contains-green/sugar-green.svg';
+export { ReactComponent as VeganIcon} from '../icons/contains-green/vegan-green.svg';
+
+// RedIcons
+export { ReactComponent as Eggs} from '../icons/contains-red/eggs-red.svg';
+export { ReactComponent as Fats} from '../icons/contains-red/fats-red.svg';
+export { ReactComponent as Fish} from '../icons/contains-red/fish-red.svg';
+export { ReactComponent as Gluten} from '../icons/contains-red/gluten-red.svg';
+export { ReactComponent as Lactose} from '../icons/contains-red/lactose-red.svg';
+export { ReactComponent as Nuts} from '../icons/contains-red/nuts-red.svg';
+export { ReactComponent as Peanuts} from '../icons/contains-red/peanuts-red.svg';
+export { ReactComponent as Sugar} from '../icons/contains-red/sugar-red.svg';
+export { ReactComponent as Vegan} from '../icons/contains-red/vegan-red.svg';
+
