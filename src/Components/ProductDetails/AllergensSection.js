d7247576a8dee2987fff3d980e96a0d33c56dc49
Sofia Duran <durannsofia@gmail.com>
2020-11-13 09:55:47 +0100

    1

--- src/Components/ProductDetails/AllergensSection.js
+++ src/Components/ProductDetails/AllergensSection.js
@@ -1,37 +1,36 @@
 import React from "react";
-// import {
-//   Eggs,
-//   Fats,
-//   Fish,
-//   Gluten,
-//   Lactose,
-//   Nuts,
-//   Peanuts,
-//   Sugar,
-//   Vegan,
-// } from "../../pages/shared/icons";
-import { allergensIcons } from "../../pages/shared/AllergensContainsData";
+import {
+  Eggs,
+  Fish,
+  Gluten,
+  Lactose,
+  Nuts,
+  Peanuts,
+} from "../../pages/shared/icons";
+import "../ProductDetails/Productdetails.css";
 
 export default function AllergensSection(props) {
   let allergensList = props.allergens;
-  console.log("allergensList: " + allergensList);
+
+  const allergensMap = new Map();
+  allergensMap
+    .set("en:eggs", <Eggs />)
+    .set("en:nuts", <Nuts />)
+    .set("en:milk", <Lactose />)
+    .set("en:gluten", <Gluten />)
+    .set("en:peanuts", <Peanuts />)
+    .set("en:fish", <Fish />);
+
   return (
-    <div>
-      {allergensList?.map((ingredient) => (
-        <p>{allergensIcons[ingredient]}</p>
-      ))}
+    <div className="allergens-section">
+      {allergensList?.map((allergen) => {
+        return (
+          <div className="allergen-icon">{allergensMap.get(allergen)}</div>
+        );
+      })}
     </div>
   );
 }
 
-// <h4>allergens icons</h4>
-//       <Eggs />
-//       <Fats />
-//       <Fish />
-//       <Gluten />
-//       <Lactose />
-//       <Nuts />
-//       <Peanuts />
 //       <Sugar />
 /* Sofi note: I have to fix the sugar icon */
-/* <Vegan /> */
