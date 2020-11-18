ad6aedf847beeca15f6436af8e5687f3dc5f322c
Omar Dieh <omardieh@gmail.com>
2020-11-18 23:07:44 +0100

    ingredients added

---
+++ src/Components/Ingredients/IngredientsElements.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const IngredientsContainer = styled.div`
+    display: inline-block;
+    width: auto;
+    height: auto;
+    justify-content: center;
+    justify-self: center;
+    align-self: center;
+    overflow: hidden;
+    vertical-align: text-top;
+    
+`
+
+export const IngredientsTable = styled.div`
+    text-align: start;
+    width: 200px;
+    padding: auto;
+    margin: auto;
+`
