8d353fcbe90f5ee1299f154cd1518ac624fc6008
Duran Sofia <durannsofia@gmail.com>
2020-11-26 23:54:54 +0100

    styled scan page

--- src/pages/ScanPage.js
+++ src/pages/ScanPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import BarCodeReader from "../Components/BarCodeReader/BarCodeReader";
+import Footer from "../Components/Footer/Footer";
 
 export default class ScanPage extends Component {
   render() {
     return (
       <div>
+        <h2>Scan the product barcode:</h2>
         <BarCodeReader />
+        <Footer />
       </div>
     );
   }
